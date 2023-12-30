85a76b8919461d32ae5dd667a1a5704cc68f9818
kaznak <kaznak.at.work@ipebble.org>
2023-12-30 11:31:02 +0900

    postgraphile# update

--- packages/postgraphile/server.ts
+++ packages/postgraphile/server.ts
@@ -1,25 +1,34 @@
 #!/usr/bin/env -S npx ts-node
 
 import "dotenv/config";
-import Fastify from "fastify";
+import Fastify, { RouteHandlerMethod } from "fastify";
 
-/*****************************************************/
+/**************************************************************
+ * Configuration
+ **************************************************************/
 const config = {
   port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
 };
 
-/*****************************************************/
+/**************************************************************
+ * Create the server and middlewares
+ **************************************************************/
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.get("/", async (request, reply) => {
+const helloMiddleware: RouteHandlerMethod = async (_request, _reply) => {
   return { hello: "world" };
-});
+};
 
-/**
+/**************************************************************
+ * Define the routes
+ **************************************************************/
+fastify.get("/", helloMiddleware);
+
+/**************************************************************
  * Run the server!
- */
+ **************************************************************/
 const start = async () => {
   try {
     await fastify.listen({ port: config.port });
